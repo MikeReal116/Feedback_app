cc37a536e0ee96f4103d30943769174383e83e7c
Michael <mickampo@yahoo.com>
2021-03-29 18:02:53 +0300

    complete server oath flow

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const keys = require("./config/keys");
+const passport = require("passport");
+require("./models/Users");
+require("./services/passport");
 
-app.get("/", (req,res)=>{
-    res.send({"hi": "hi there"});
-})
+mongoose.connect(keys.mongoURI);
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+const app = express()
+
+app.use(cookieSession({
+    maxAge : 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT)
